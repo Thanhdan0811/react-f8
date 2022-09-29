c7a9440a3625aa2bfd3ab7c058a05d295817f8be
Dan <ngthdan0811@gmail.com>
2022-09-29 20:41:52 +0700

    update useRef

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -2,14 +2,35 @@ import { useRef, useEffect, useState } from "react";
 
 function Content() {
   const [count, setCount] = useState(60);
+  // const ref = useRef(99);
 
-  const handleStart = () => {};
+  const timerId = useRef();
+  const preCount = useRef();
+  const h1Ref = useRef();
 
-  const handleStop = () => {};
+  useEffect(() => {
+    preCount.current = count;
+  }, [count]);
+
+  useEffect(() => {
+    console.loog(h1Ref);
+  });
+
+  const handleStart = () => {
+    timerId.current = setInterval(() => {
+      setCount((prevCount) => prevCount - 1);
+    }, 1000);
+  };
+
+  const handleStop = () => {
+    clearInterval(timerId.current);
+  };
+
+  console.log(count, preCount.current);
 
   return (
     <div style={{ padding: 20 }}>
-      <h1>{count}</h1>
+      <h1 ref={h1Ref}>{count}</h1>
       <button onClick={handleStart}>Start</button>
       <button onClick={handleStop}>Stop</button>
     </div>
